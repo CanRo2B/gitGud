82857a99ffda075ca41af14b2f150cf8897d60a7
Amalie Von Behren <vbamalie@gmail.com>
2022-04-11 16:49:56 -0400

    function that actually fetches data

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
 //authorization keys needed to create the actual access token
 const twitchClientId = "ddg5ztvzrbtcgwze0t9jbb6wqn5dj0";
 const twitchSecretId= "axxonlvfp1hw6c4omorwefqwjno7o0";
+var endpoint = "https://api.twitch.tv/helix/streams"
 
 //this function makes the access token that is recquired each time we fetch from twitch
 function getTwitchAuthorization(){
@@ -16,7 +17,31 @@ function getTwitchAuthorization(){
 }
 
 async function testing(){
-    console.log(await getTwitchAuthorization());
+    
+    let tokenObject = await getTwitchAuthorization();
+    console.log(tokenObject)
+    
+    let accesToken = tokenObject.access_token;
+    let bearer = tokenObject.token_type;
+
+    console.log(accesToken);
+    console.log(bearer)
+    
+    bearer = bearer.substring(0,1).toUpperCase() + bearer.substring(1,bearer.length);
+
+    let authorization = `${bearer} ${accesToken}`
+
+    let headers = {
+        authorization, "client-Id": twitchClientId
+    };
+
+    fetch(endpoint , {headers})
+    .then(function(respond){
+        return respond.json()
+    })
+    .then(function(data){
+        console.log(data);
+    });
 }
 
-testing();
+testing()
