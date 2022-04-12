1bde2114141926bd7080090fbf23407ee35561df
Amalie Von Behren <vbamalie@gmail.com>
2022-04-12 14:56:54 -0400

    twitch Auth gives out id

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,41 @@
-//authorization keys needed to create the actual access token
+const gamesArray =[]
+
+//placeholder variables until user input is hooked up
+let platformVar = "pc"
+let categoryVar = "shooter"
+
+categoryOptions= ["shooter.first-person" , "shooter.battle-royale" , "open-world" , "mmorpg" , "mmofps" , "fighting" , "strategy" , "tower-defense"]
+
+function free2GameFetch(platform, category,){
+    var url = `https://floating-headland-95050.herokuapp.com/https://www.freetogame.com/api/games?platform=${platform}&tag=${category}&sort-by=popularity`
+
+    return fetch(url)
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        //console.log(data)
+        return data
+    })
+}
+
+// function chooseGenre(event){
+//     var genre = event.${"event"}
+// }
+
+async function createGameList(){
+gameFetch = await free2GameFetch(platformVar , categoryVar)
+for (i = 0; i < 10; i++){
+    gamesArray.push(gameFetch[i].title)
+}
+console.log(gamesArray)
+}
+
+createGameList();
+
+//var language = event.target.getAttribute('data-language');
+
+
 const twitchClientId = "ddg5ztvzrbtcgwze0t9jbb6wqn5dj0";
 const twitchSecretId= "axxonlvfp1hw6c4omorwefqwjno7o0";
 var twitchUrl = "https://api.twitch.tv/helix/"
@@ -22,13 +59,6 @@ function getTwitchAuthorization(){
 var streamEndpoint = "streams?first=5&game_id"
 var gameEndpoint = "games?name=Fortnite"
 
-testing()
-async function testing(){
-var gameArray = twitchGrab(gameEndpoint)
-gameId = await gameArray[0].id;
-console.log(gameId)
-};
-
 async function twitchGrab(endpoint){
     
     let tokenObject = await getTwitchAuthorization();
@@ -50,12 +80,20 @@ async function twitchGrab(endpoint){
 
     twitchEndpoint = twitchUrl + endpoint
 
-    fetch(twitchEndpoint , {headers})
+    return fetch(twitchEndpoint , {headers})
     .then(function(respond){
         return respond.json()
     })
     .then(function(data){
-        console.log(data);
+        return data;
     });
 }
 
+async function fetchGameId(){
+    var twitchData = await twitchGrab(gameEndpoint)
+    console.log(twitchData)
+    gameId = twitchData.data[0].id
+    console.log(gameId)
+}
+
+fetchGameId()
