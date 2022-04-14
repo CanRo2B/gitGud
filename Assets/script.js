f87519aba92a1bc3214f935e537be4b7007408e9
GigaScott <scott.gamit.09@outlook.com>
2022-04-14 09:52:59 -0400

    placed for loop into correct function

--- Assets/script.js
+++ Assets/script.js
@@ -5,6 +5,7 @@ const gameImageEl = document.querySelector("#gameImage");
 const carouselEl = $("#top5");
 const infoC = $("#gameInfo");
 const olEl = $("#games");
+const topTwitch = $("#carousel-demo");
 
 //global variables for the twitch authorization
 const twitchClientId = "ddg5ztvzrbtcgwze0t9jbb6wqn5dj0";
@@ -19,6 +20,34 @@ var formEl = $("#gameFind");
 async function getStreamInfo(id) {
     var fullEndpoint = `streams?first=5&game_id=${id}`
     var twitchData = await twitchGrab(fullEndpoint);
+
+    for (var i = 0; i < 5; i++) {
+        var newDiv = document.createElement('div');
+        newDiv.id = 'streams';
+        newDiv.setAttribute("class", "item-" + i)
+        var userName = document.createElement('h4');
+        var liveStatus = document.createElement('p');
+        var viewercount = document.createElement('p');
+        var link = document.createElement('a');
+        var thumbnail = document.createElement("iframe");
+        thumbnail.setAttribute("src", "https://player.twitch.tv/?channel=" + twitchData.data[i].user_name + "&parent=https://canro2b.github.io/")
+        thumbnail.frameBorder = 0;
+        thumbnail.allowFullscreen = "true";
+        thumbnail.style.height = 300;
+        thumbnail.style.width = 400;
+        userName.textContent = "Username: " + twitchData.data[i].user_name;
+        liveStatus.textContent = twitchData.data[i].type.toUpperCase();
+        viewercount.textContent = "Viewers: " + twitchData.data[i].viewer_count;
+        link.setAttribute('href', "https://www.twitch.tv/" + twitchData.data[i].user_name);
+        link.setAttribute("target", "_blank");
+        link.innerHTML = "https://www.twitch.tv/" + twitchData.data[i].user_name
+        topTwitch.append(newDiv);
+        newDiv.append(userName); // topTwitch will change via HTML
+        newDiv.append(link);
+        newDiv.append(liveStatus);
+        newDiv.append(viewercount);
+        newDiv.append(thumbnail);
+    }
 }
 
 
@@ -125,32 +154,3 @@ formEl.on("submit", function (event) {
     var genre = gSelected[0].dataset.genre;
     createGameList(platform, genre);
 });
-
-// for loop to pull Streamer Data 
-for (var i = 0; i < 5; i++) {
-    var newDiv = document.createElement('div');
-    // newDiv.id = 'r' + i;
-    newDiv.setAttribute("class", "item-" + i)
-    var userName = document.createElement('h4');
-    var liveStatus = document.createElement('p');
-    var viewercount = document.createElement('p');
-    var link = document.createElement('a');
-    var thumbnail = document.createElement("iframe");
-    thumbnail.setAttribute("src", "https://player.twitch.tv/?channel=" + twitchData.data[i].user_name + "&parent=www.example.com")
-    thumbnail.frameBorder = 0;
-    thumbnail.allowFullscreen = "true";
-    thumbnail.style.height = 300;
-    thumbnail.style.width = 400;
-    userName.textContent = "Username: " + twitchData.data[i].user_name;
-    liveStatus.textContent = twitchData.data[i].type.toUpperCase();
-    viewercount.textContent = "Viewers: " + twitchData.data[i].viewer_count;
-    link.setAttribute('href', "https://www.twitch.tv/" + twitchData.data[i].user_name);
-    link.setAttribute("target", "_blank");
-    link.innerHTML = "https://www.twitch.tv/" + twitchData.data[i].user_name
-    topTwitch.append(newDiv);
-    newDiv.append(userName); // topTwitch will change via HTML
-    newDiv.append(link);
-    newDiv.append(liveStatus);
-    newDiv.append(viewercount);
-    newDiv.append(thumbnail);
-}
